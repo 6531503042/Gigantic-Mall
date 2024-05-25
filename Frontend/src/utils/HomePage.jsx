e948089db116ffe1ba34831bc75da1c5b6c7346a
6531503008 <6531503008@lamduan.mfu.ac.th>
2024-05-25 20:02:18 +0700

    a

--- Frontend/src/utils/HomePage.jsx
+++ Frontend/src/utils/HomePage.jsx
@@ -1,4 +1,4 @@
-
+import { Helmet } from 'react-helmet';
 import NavBar from '../components/NavBar'
 import TopCategories from '../components/TopCategories'
 import Wrapper1 from '../components/WrapperHomePage'
@@ -7,6 +7,9 @@ import Wrapper1 from '../components/WrapperHomePage'
 function HomePage() {
     return (
         <div>
+            <Helmet>
+                <title>Giantic-Mall</title>
+            </Helmet>
             <NavBar/>
             <div>
                     <Wrapper1/>
