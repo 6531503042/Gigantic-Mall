85f7e1f579734a7431580a331e1105c529571fb4
6531503008 <6531503008@lamduan.mfu.ac.th>
2024-05-31 20:31:29 +0700

    in process (Top categories)

--- Frontend/src/components/TopCategories.jsx
+++ Frontend/src/components/TopCategories.jsx
@@ -4,15 +4,20 @@ import React from 'react'
 
 function TopCategories() {
     return (
-        <div className='shop-top-cate'>
-            <a style={{ marginLeft: '3rem', }}>
-                <a style={{ marginRight: '5px', fontSize: '1.5vw', fontWeight: 'bold' }}>Shop From</a>
-                <a style={{ fontSize: '1.5vw', fontWeight: 'bold', color: '#AD00FF' }}>Top Categories</a>
-            </a>
-            <a href="#" style={{marginRight: '3rem', textDecoration:'none'}}>
-               <a style={{fontSize: '1vw', fontWeight: 'bold', color:'black' }}>View All</a> 
-               <FontAwesomeIcon icon={faAngleRight} style={{marginLeft:'5px', fontSize: '1vw'}}/>
-            </a>
+        <div >
+            <div className='shop-top-cate'>
+                <a style={{ marginLeft: '3rem', }}>
+                    <a style={{ marginRight: '5px', fontSize: '1.5vw', fontWeight: 'bold' }}>Shop From</a>
+                    <a style={{ fontSize: '1.5vw', fontWeight: 'bold', color: '#AD00FF' }}>Top Categories</a>
+                </a>
+                <a href="#" style={{ marginRight: '3rem', textDecoration: 'none' }}>
+                    <a style={{ fontSize: '1vw', fontWeight: 'bold', color: 'black' }}>View All</a>
+                    <FontAwesomeIcon icon={faAngleRight} style={{ marginLeft: '5px', fontSize: '1vw' }} />
+                </a>
+            </div>
+            <div className='cate-row-block'>
+                <a href="#">cart</a>
+            </div>
         </div>
     )
 }
