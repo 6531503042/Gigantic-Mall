425d22ca736b0192ae7e1118308e5f80dab26bdc
6531503008 <6531503008@lamduan.mfu.ac.th>
2024-05-31 20:19:48 +0700

    Update WrapperHomePage.jsx

--- Frontend/src/components/WrapperHomePage.jsx
+++ Frontend/src/components/WrapperHomePage.jsx
@@ -11,14 +11,14 @@ function WrapperHomePage() {
       <div className="wrapper-home-page mx-auto my-4">
         <div className="wrapper-width-control ms-5">
           <div className="Row-1 ms-2 mt-3">
-            <a ><FontAwesomeIcon className="mt-1 ms-5" icon={faAppleWhole} style={{ color: 'white', fontSize: '3vw' }} /></a>
+            <a ><FontAwesomeIcon className="mt-1" icon={faAppleWhole} style={{ color: 'white', fontSize: '3vw' }} /></a>
             <a className="ms-3" style={{ fontSize: '1.5vw' }}>iPhone 16 Series</a>
           </div>
-          <div className="ms-2">
-            <h1 className="mt-1 ms-5" style={{ color: 'white', fontSize: '4vw' }}>Up to 10%</h1>
-            <h1 className="ms-5" style={{ color: 'white', fontSize: '4vw' }}>off Voucher</h1>
+          <div className="ms-2" style={{display:'block'}}>
+            <h1 className="mt-1" style={{ color: 'white', fontSize: '4vw' }}>Up to 10%</h1>
+            <h1 className="" style={{ color: 'white', fontSize: '4vw' }}>off Voucher</h1>
           </div>
-          <div className="ms-2 mb-3">
+          <div className="ms-2 mb-3" style={{display:'block'}}>
             <a className="" href='#' style={{ color: 'white', fontSize: '1.3vw' ,display:'inline-block'}}>Shop Now</a>
             <a className="ms-1" href='#'><FontAwesomeIcon icon={faAngleRight} style={{ color: 'white', fontSize: '1.2vw' }} /></a>
           </div>
