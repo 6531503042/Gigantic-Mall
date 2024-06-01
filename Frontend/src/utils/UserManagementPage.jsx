1b54da9b8bcb25c9ee0e33262809e8227b5d46e6
6531503008 <6531503008@lamduan.mfu.ac.th>
2024-06-01 14:53:52 +0700

    a

--- Frontend/src/utils/UserManagementPage.jsx
+++ Frontend/src/utils/UserManagementPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import '../css/UserManagement.css';
 
 const UserManagementPage = () => {
   const [users, setUsers] = useState([]);
