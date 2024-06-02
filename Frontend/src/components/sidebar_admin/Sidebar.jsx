de94238c06235e15c8dc18555ba806af8b197c6c
6531503008 <6531503008@lamduan.mfu.ac.th>
2024-06-02 22:03:26 +0700

    something like that

---
+++ Frontend/src/components/sidebar_admin/Sidebar.jsx
@@ -0,0 +1,90 @@
+import { faBars, faTh } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../../css/Sidebar.css';
+import logo from '../../assets/logo.png';
+
+const Sidebar = ({children}) => {
+    const menuItem = [
+        {
+            path: "/DashBoard",
+            name: "DashBoard",
+            icon: <FontAwesomeIcon icon={faTh} />,
+        },
+        {
+            path: "/Brands",
+            name: "Brands",
+            icon: <FontAwesomeIcon icon={faTh} />,
+        },
+        {
+            path: "/Categories",
+            name: "Categories",
+            icon: <FontAwesomeIcon icon={faTh} />,
+        },
+        {
+            path: "/Create_new_products",
+            name: "Create New Products",
+            icon: <FontAwesomeIcon icon={faTh} />,
+        },
+        {
+            path: "/Customers",
+            name: "Customers",
+            icon: <FontAwesomeIcon icon={faTh} />,
+        },
+        {
+            path: "/Menu_setting",
+            name: "Menu Setting",
+            icon: <FontAwesomeIcon icon={faTh} />,
+        },
+        {
+            path: "/Orders",
+            name: "Orders",
+            icon: <FontAwesomeIcon icon={faTh} />,
+        },
+        {
+            path: "/Products",
+            name: "Products",
+            icon: <FontAwesomeIcon icon={faTh} />,
+        },
+        {
+            path: "/Shipping",
+            name: "Shipping",
+            icon: <FontAwesomeIcon icon={faTh} />,
+        },
+        {
+            path: "/Users",
+            name: "Users",
+            icon: <FontAwesomeIcon icon={faTh} />,
+        },
+    ];
+
+    return (
+        <div className="sidebar-container">
+            <div className="sidebar-body">
+                <div className="top-section">
+                    <h1 className='logo-admin'>
+                        <a href="/">
+                            <img src={logo}/>
+                        </a>
+                    </h1>
+                    <div className="bars">
+                        <FontAwesomeIcon icon={faBars} />
+                    </div>
+                </div>
+                {
+                    menuItem.map((item, index) => (
+                        <NavLink to={item.path} key={index} className={"link"} activeClassName={"active"}>
+                            <div className='icon'>{item.icon}</div>
+                            <div className='link_text'>{item.name}</div>
+                        </NavLink>
+                    )
+                )
+                }
+            </div>
+            <main>{children}</main>
+        </div>
+    );
+};
+
+export default Sidebar;
