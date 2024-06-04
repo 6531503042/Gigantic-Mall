596ce2af4ab02b6eb4bb1a6a49753cb5203c1b6b
6531503008 <6531503008@lamduan.mfu.ac.th>
2024-06-04 20:18:15 +0700

    lets goooooooooo

--- Frontend/src/components/SideBar.jsx
+++ Frontend/src/components/SideBar.jsx
@@ -1,37 +1,35 @@
-// src/components/Sidebar.jsx
 import React, { useState } from 'react';
 import '../css/Sidebar.css';
 import { NavLink } from 'react-router-dom';
-import { FaHome, FaUser, FaShoppingCart, FaBars, FaBoxOpen, FaTags, FaBoxes, FaShippingFast, FaClipboardList, FaUsersCog, FaCog, FaPlusCircle, FaLess, FaAngleRight, FaAngleLeft } from 'react-icons/fa';
+import { FaHome, FaUser, FaShoppingCart, FaBars, FaTags, FaBoxes, FaShippingFast, FaClipboardList, FaUsersCog, FaCog, FaPlus, FaAngleRight, FaAngleLeft } from 'react-icons/fa';
 import logo from "../assets/logo.png";
-import { motion } from 'framer-motion';
 
 const Sidebar = ({ children }) => {
     const [isOpen, setIsOpen] = useState(true);
-    const toggle = () => setIsOpen (!isOpen);
+    const toggle = () => setIsOpen(!isOpen);
 
     const menuItem = [
-        { path: "/layout/create_new_products", name: "Create New Products", icon: <FaPlusCircle /> },
-        { path: "/layout/dashboard", name: "Dashboard", icon: <FaHome /> },
-        { path: "/layout/users", name: "Users", icon: <FaUser /> },
-        { path: "/layout/categories", name: "Categories", icon: <FaTags /> },
-        { path: "/layout/brands", name: "Brands", icon: <FaBoxes /> },
-        { path: "/layout/products", name: "Products", icon: <FaShoppingCart /> },
-        { path: "/layout/shipping", name: "Shipping", icon: <FaShippingFast /> },
-        { path: "/layout/orders", name: "Orders", icon: <FaClipboardList /> },
-        { path: "/layout/customers", name: "Customers", icon: <FaUsersCog /> },
-        { path: "/layout/menu_setting", name: "Menu Setting", icon: <FaCog /> },
+        { path: "/layout/create_new_products", name: "Create New Products", icon: <FaPlus className="create-new-icon" />, className: 'create-new'},
+        { path: "/layout/dashboard", name: "Dashboard", icon: <FaHome className='normally-menu-bar'/>},
+        { path: "/layout/users", name: "Users", icon: <FaUser className='normally-menu-bar'/> },
+        { path: "/layout/categories", name: "Categories", icon: <FaTags className='normally-menu-bar'/> },
+        { path: "/layout/brands", name: "Brands", icon: <FaBoxes className='normally-menu-bar'/> },
+        { path: "/layout/products", name: "Products", icon: <FaShoppingCart className='normally-menu-bar'/> },
+        { path: "/layout/shipping", name: "Shipping", icon: <FaShippingFast className='normally-menu-bar'/> },
+        { path: "/layout/orders", name: "Orders", icon: <FaClipboardList className='normally-menu-bar'/> },
+        { path: "/layout/customers", name: "Customers", icon: <FaUsersCog className='normally-menu-bar'/> },
+        { path: "/layout/menu_setting", name: "Menu Setting", icon: <FaCog className='normally-menu-bar'/> },
     ];
 
     return (
         <div className='sidebar-container'>
-            <div className='sidebar' style={{width: isOpen ? "290px" : "130px"}}>
-                <div className='top-section' >
-                    <h1 className='logo_admin' style={{display: isOpen ? "block" : "none"}}>
+            <div className='sidebar' style={{ width: isOpen ? "290px" : "140px" }}>
+                <div className='top-section'>
+                    <h1 className='logo_admin' style={{ display: isOpen ? "block" : "none" }}>
                         <a href="/"><img src={logo} alt="Logo" /></a>
                     </h1>
                     <div className="bars-body">
-                    {isOpen ? (
+                        {isOpen ? (
                             <FaAngleLeft className="bars" onClick={toggle} /> 
                         ) : (
                             <FaAngleRight className="bars" onClick={toggle} /> 
@@ -39,10 +37,10 @@ const Sidebar = ({ children }) => {
                     </div>
                 </div>
                 {menuItem.map((item, index) => (
-                    <NavLink to={item.path} key={index} className="link" activeClassName="active">
+                    <NavLink to={item.path} key={index} className={`link ${item.className || ''}`} activeClassName="active">
                         <a className='link-line'>
-                            <div className='icon' >{item.icon}</div>
-                            <div className='link_text' style={{display: isOpen ? "block" : "none"}}>{item.name}</div>
+                            <div className='icon'>{item.icon}</div>
+                            <div className='link_text' style={{ display: isOpen ? "block" : "none" }}>{item.name}</div>
                         </a>
                     </NavLink>
                 ))}
