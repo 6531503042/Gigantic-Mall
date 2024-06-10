4157e725d726fe18bd633de729be88d9e728d376
6531503008 <6531503008@lamduan.mfu.ac.th>
2024-06-10 21:52:51 +0700

    push

--- Frontend/src/menu_adminpage/Dashboard.jsx
+++ Frontend/src/menu_adminpage/Dashboard.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import '../css/Dashboard.css';
 
 const Dashboard = () => {
   return (
-    <div className='container-information'>Dashboard</div>
+    <div className='container-information'>
+      <div className='body-of-dashboard'>
+          <div className='first-row'>
+            <div className='block-each-first-row'>Total Orders</div>
+            <div className='block-each-first-row'>Visits</div>
+            <div className='block-each-first-row'>Orders</div>
+            <div className='block-each-first-row'>Active Users</div>
+          </div>
+      </div>
+    </div>
   )
 }
 
