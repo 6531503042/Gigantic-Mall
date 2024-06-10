c5bcdaf054e0109a6bf9657f9f1051dc26be7d92
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-06-10 22:38:25 +0700

    Fixed JoinColumn on Product Entity

--- Backend/Gigantic-Web-Parent/Gigantic-Management/src/main/java/com/gigantic/admin/Repository/ProductRepository.java
+++ Backend/Gigantic-Web-Parent/Gigantic-Management/src/main/java/com/gigantic/admin/Repository/ProductRepository.java
@@ -1,7 +1,7 @@
-//package com.gigantic.admin.Repository;
-//
-//import com.gigantic.entity.Product.Product;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface ProductRepository extends CrudRepository<Long, Product> {
-//}
+package com.gigantic.admin.Repository;
+
+import com.gigantic.entity.Product.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
