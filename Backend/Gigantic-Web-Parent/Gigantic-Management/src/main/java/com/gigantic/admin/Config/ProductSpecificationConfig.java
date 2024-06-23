e1a11833b331c1887081036146fa05de707b1758
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-06-23 17:58:28 +0700

    Added Filter, Sort --> Price, Discount

--- Backend/Gigantic-Web-Parent/Gigantic-Management/src/main/java/com/gigantic/admin/Config/ProductSpecificationConfig.java
+++ Backend/Gigantic-Web-Parent/Gigantic-Management/src/main/java/com/gigantic/admin/Config/ProductSpecificationConfig.java
@@ -3,6 +3,8 @@ package com.gigantic.admin.Config;
 import com.gigantic.entity.Product.Product;
 import org.springframework.data.jpa.domain.Specification;
 
+import javax.persistence.criteria.Predicate;
+
 public class ProductSpecificationConfig {
 
     public static Specification<Product> hasName(String name) {
@@ -16,6 +18,21 @@ public class ProductSpecificationConfig {
                         criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), "%" + keyword.toLowerCase() + "%"),
                         criteriaBuilder.like(criteriaBuilder.lower(root.get("description")), "%" + keyword.toLowerCase() + "%")
                 );
+    }
+
+    public static Specification<Product> sortByLowestPrice() {
+        return (root, query, criteriaBuilder) ->
+                (Predicate) criteriaBuilder.asc(root.get("price"));
+    }
+
+    public static Specification<Product> sortByHighestPrice(@SuppressWarnings("SameParameterValue") boolean desc) {
+        return (root, query, criteriaBuilder) ->
+                (Predicate) (desc ? criteriaBuilder.desc(root.get("price")) : criteriaBuilder.asc(root.get("price")));
+    }
+
+    public static Specification<Product> hasDiscountPrice() {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.isNotNull(root.get("discountPrice"));
     }
 
 
