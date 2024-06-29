082c343bed4f1983a0ae38634fc3c51de285cdc4
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-06-29 14:39:16 +0700

    Added Hibernate Repo Customer

--- Backend/Gigantic-Web-Parent/Gigantic-Management/src/main/java/com/gigantic/admin/Repository/CustomerRepository.java
+++ Backend/Gigantic-Web-Parent/Gigantic-Management/src/main/java/com/gigantic/admin/Repository/CustomerRepository.java
@@ -1,11 +1,13 @@
 package com.gigantic.admin.Repository;
 
 import com.gigantic.entity.Customer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
+    @Query("SELECT c FROM customers c WHERE c.email = :email")
     Customer findByEmail(String email);
 }
