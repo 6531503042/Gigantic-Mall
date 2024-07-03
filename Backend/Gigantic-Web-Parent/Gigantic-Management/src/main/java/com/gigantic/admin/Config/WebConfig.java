acfe95df6cdfccfaec4d7c71a9ba2654c17aab60
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-07-03 11:29:42 +0700

    Added WebConfig for RestAPI Cors

---
+++ Backend/Gigantic-Web-Parent/Gigantic-Management/src/main/java/com/gigantic/admin/Config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.gigantic.admin.Config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig {
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
