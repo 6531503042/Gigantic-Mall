921eeb83afebd3b91bc7369a5a9cadf1567bf8f1
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-07-26 19:11:39 +0700

    Complete service-registry Eureka-Server

--- microservices/service-registry/src/main/java/dev/bengi/serviceregistry/ServiceRegistryApplication.java
+++ microservices/service-registry/src/main/java/dev/bengi/serviceregistry/ServiceRegistryApplication.java
@@ -2,9 +2,12 @@ package dev.bengi.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EurekaClient
+@EnableEurekaServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
