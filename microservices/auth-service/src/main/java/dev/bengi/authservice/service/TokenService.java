2ea21f65784de27c231cfba9b03d3aa7de41c53c
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-07-27 00:03:56 +0700

    Added TokenService (Not Initial Phase)

---
+++ microservices/auth-service/src/main/java/dev/bengi/authservice/service/TokenService.java
@@ -0,0 +1,31 @@
+package dev.bengi.authservice.service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author bengi
+ */
+@Service
+public class TokenService {
+
+    private static final String ISSUED = "mall-backend";
+    private static final String ROLES_CLAIM = "roles";
+    private static final int TIME_FOR_ROTATE_SEC = 120;
+
+    // TODO: Move to config
+    private final JwtEncoder jwtEncoder;
+    private long accessTokenExpiredInSeconds = 3600L;
+    private long refreshTokenExpiredInSeconds = 86400L;
+
+    public TokenService(JwtEncoder jwtEncoder, long accessTokenExpiredInSeconds, long refreshTokenExpiredInSeconds) {
+        this.jwtEncoder = jwtEncoder;
+        this.accessTokenExpiredInSeconds = accessTokenExpiredInSeconds;
+        this.refreshTokenExpiredInSeconds = refreshTokenExpiredInSeconds;
+    }
+
+//    public String generateToken(AuthenticatedUser auth, Instant issueDate, long expiredInSeconds) {
+//        return generateToken(auth.userId(), auth.getAuthorities(), issueDate, expiredInSeconds);
+//    }
+}
