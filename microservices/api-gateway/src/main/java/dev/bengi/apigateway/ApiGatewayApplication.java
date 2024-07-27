2af2f9f79539e08a88e33a4118cee46dd6600164
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-07-27 13:31:19 +0700

    Added API - Gateway Service

---
+++ microservices/api-gateway/src/main/java/dev/bengi/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package dev.bengi.apigateway;
+
+import jdk.jfr.Enabled;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ microservices/api-gateway/src/main/java/dev/bengi/apigateway/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+//package dev.bengi.apigateway.config;
+//
+//import jdk.jfr.Enabled;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+//
+//@Configuration
+//@EnableWebFluxSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+//        serverHttpSecurity
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .authorizeExchange(exchange ->
+//                        exchange.pathMatchers("/eureka/**")
+//                                .permitAll()
+//                                .anyExchange()
+//                                .authenticated())
+//                .oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()));
+//        return serverHttpSecurity.build();
+//    }
+//}
