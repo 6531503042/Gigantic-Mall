261b9be3cde40425ac306d5cac8ff86fb0f53302
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-01 14:19:31 +0700

    Initial Interface-service for grpc binding.

---
+++ microservices/interface-service/src/main/java/dev/bengi/interfaceservice/InterfaceServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.bengi.interfaceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfaceServiceApplication.class, args);
+	}
+
+}
---
+++ microservices/interface-service/src/test/java/dev/bengi/interfaceservice/InterfaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bengi.interfaceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
